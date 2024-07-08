a1fcef73c02030af4e6848bc19b80a1f510cb548
Mouhanad Ahmed <mouhanad.aboelmakarem@gmail.com>
2024-07-08 20:03:46 +0300

    updated server location for render

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import  dbConnection  from './Database/dbConnection.js';
-import  init  from './src/index.js';
+import  dbConnection  from '../Database/dbConnection.js';
+import  init  from './index.js';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -9,7 +9,6 @@ dotenv.config();
 
 const app = express()
 const port = 8080
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads',express.static("uploads"))           // Serving the static uploads
 app.use(cors())                              // Enabling CORS globally on the server
 app.use(express.json());                     //Enabling a built in middleware function in Express . It parses incoming JSON requests and puts the parsed data in req.body
